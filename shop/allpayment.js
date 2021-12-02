f2488ff219f22f1953637619b96565ab95ff358c
MadeSparkle <https://github.com/MadeSparkle/Adam.git>
2021-12-03 06:27:43 +0700

    ngetes

---
+++ shop/allpayment.js
@@ -0,0 +1,24 @@
+const allpayment = (prefix) => {
+return `PAYMENT BY BOTZ V1
+
+	
+*💰 E-MONEY*
+
+1. GOPAY
+	ㅁ -
+2. OVO
+	ㅁ -
+3. DANA
+	ㅁ 0822 3256 8963
+	
+
+*📲 PULSA*
+
+Tidak tersedia
+	
+Sebelum melakukan pembayaran ada baiknya anda menghubungi owner terlebih dahulu!
+`
+	}
+
+exports.allpayment = allpayment
+ 
---
+++ shop/donasi.js
@@ -0,0 +1,20 @@
+const donasibot = () => {
+return `DONASI E-MONEY
+
+
+	
+*💰 E-MONEY*
+
+1. GOPAY
+	ㅁ 0887 4350 47326(Creator)
+2. OVO
+	ㅁ 0887 4350 47326 (Creator)
+3. DANA
+	ㅁ 0822 3256 8963 (Recode/Owner)
+	
+Thankyou
+`
+	}
+
+exports.donasibot = donasibot
+ 
